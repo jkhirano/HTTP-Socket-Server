663d5032b5b750fbc819e0be5a1707c886dc91e0
Janelle K Hirano <jaahnelle@gmail.com>
2019-08-10 09:55:00 -1000

    added index, hydrogen, helium, notfound

--- server.js
+++ server.js
@@ -2,33 +2,82 @@
 
 const net = require("net");
 const files = require("./files.js");
-
 const PORT = 8080;
 
+let date = "";
+let body = "";
+
+//this creates a server
 const server = net.createServer(socket => {
   socket.on("data", chunk => {
-    // read incoming data
+    // read incoming data (incoming chunk is request header)
     console.log("data");
     console.log(chunk.toString());
 
     // parse the string
+    let request = chunk.toString().slice(0, chunk.indexOf("H") - 1);
+    console.log(request);
 
     // grab the right file
+    if (request === "GET /index.html") {
+      body = files.index;
+      let response = `HTTP/1.1 200 OK
+Server: localhost:8080
+Date: ${date}
+Content-Type: text/html;
+Content-Length: ${body.length}
 
-    // write outgoing data
-    socket.write("test");
-    socket.end();
-  });
+${body}`;
+      socket.write(response);
+      socket.end();
+    } else if (request === "GET /hydrogen.html") {
+      body = files.hydrogen;
+      let response = `HTTP/1.1 200 OK
+Server: localhost:8080
+Date: ${date}
+Content-Type: text/html;
+Content-Length: ${body.length}
 
-  socket.on("end", () => {
-    // handle client disconnect
-  });
+${body}`;
+      socket.write(response);
+      socket.end();
+    } else if (request === "GET /helium.html") {
+      body = files.helium;
+      let response = `HTTP/1.1 200 OK
+Server: localhost:8080
+Date: ${date}
+Content-Type: text/html;
+Content-Length: ${body.length}
 
-  socket.on("error", err => {
-    // handle error in connection
+${body}`;
+      socket.write(response);
+      socket.end();
+    } else if (request === "GET /notFound.html") {
+      body = files.notFound;
+      let response = `HTTP/1.1 404 NOT FOUND
+Server: localhost:8080
+Date: ${date}
+Content-Type: text/html;
+Content-Length: ${body.length}
+
+${body}`;
+      socket.write(response);
+      socket.end();
+    }
+
+    socket.on("end", () => {
+      // handle client disconnect
+      console.log("Connection disconnected");
+    });
+
+    socket.on("error", err => {
+      // handle error in connection
+      console.log(err);
+    });
   });
 });
 
+// this starts the server
 server.listen(PORT, () => {
   console.log(`Server Running on ${PORT}`);
 });
